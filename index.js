2c1deab589748f5dbf84be66fe8790ce3b8a332e
ezhil6006 <san.ezhil@gmail.com>
2015-08-19 17:41:03 +0530

    commit ezhil

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var _ip = require("ip");
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+   response.end('Hello World\n');
+}).listen(5000);
+
+// Console will print the message
+console.log('Server running at http://'+_ip.address()+':5000/');
